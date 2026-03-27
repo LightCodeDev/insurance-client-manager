ca419fdfd7d8ed272d8b02f0a1ed82106be69964
LightCodeDev <daniguzman202202@gmail.com>
2026-03-27 03:18:28 -0600

    feat: test MySQL connection and basic SELECT query

---
+++ src/test_db.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+conexion = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="***********",
+    database="saas_seguros"
+)
+
+cursor = conexion.cursor()
+cursor.execute("SELECT * FROM clientes")
+
+resultados = cursor.fetchall()
+
+for cliente in resultados:
+    print(cliente)
+
+cursor.close()
+conexion.close()
