7ddf1062b35895c57dc3256a182b62aa761600a6
LightCodeDev <daniguzman202202@gmail.com>
2026-04-03 20:19:18 -0600

    chore: update environment configuration and dependencies

--- src/config.py
+++ src/config.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_NAME = os.getenv("DB_NAME", "saas_seguros")
 
 
 TIPOS_SEGURO = ["hogar","auto","vida","medico","robo","decesos","accidentes"]
